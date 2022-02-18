2e23a03085255de6fcbe700a7f1d0bf86fd807c2
huangfengmiao <huangfengmiao@itiger.com>
2022-02-18 14:00:40 +0800

    fix

--- dict.go
+++ dict.go
@@ -6,61 +6,6 @@ import (
 	"time"
 )
 
-const (
-	BIG_M = 0xc6a4a7935bd1e995
-	BIG_R = 47
-	SEED  = 0x1234ABCD
-)
-
-func MurmurHash64A(data []byte) (h int64) {
-	var k int64
-	h = SEED ^ int64(uint64(len(data))*BIG_M)
-
-	var ubigm uint64 = BIG_M
-	var ibigm = int64(ubigm)
-	for l := len(data); l >= 8; l -= 8 {
-		k = int64(data[0]) | int64(data[1])<<8 | int64(data[2])<<16 | int64(data[3])<<24 |
-			int64(data[4])<<32 | int64(data[5])<<40 | int64(data[6])<<48 | int64(data[7])<<56
-
-		k := k * ibigm
-		k ^= int64(uint64(k) >> BIG_R)
-		k = k * ibigm
-
-		h = h ^ k
-		h = h * ibigm
-		data = data[8:]
-	}
-
-	switch len(data) {
-	case 7:
-		h ^= int64(data[6]) << 48
-		fallthrough
-	case 6:
-		h ^= int64(data[5]) << 40
-		fallthrough
-	case 5:
-		h ^= int64(data[4]) << 32
-		fallthrough
-	case 4:
-		h ^= int64(data[3]) << 24
-		fallthrough
-	case 3:
-		h ^= int64(data[2]) << 16
-		fallthrough
-	case 2:
-		h ^= int64(data[1]) << 8
-		fallthrough
-	case 1:
-		h ^= int64(data[0])
-		h *= ibigm
-	}
-
-	h ^= int64(uint64(h) >> BIG_R)
-	h *= ibigm
-	h ^= int64(uint64(h) >> BIG_R)
-	return
-}
-
 type Target struct {
 	DataSize      int
 	Factor        int64
@@ -91,7 +36,6 @@ func (d *Dict) migration() {
 			next := entry.Next
 			for true {
 				if entry2 := d.t2[entry.H&d.sizemask]; entry2 != nil {
-					// 新队列里有值
 					next2 := entry2.Next
 					for true {
 						if next2 == nil {
@@ -103,9 +47,7 @@ func (d *Dict) migration() {
 						entry2 = entry2.Next
 						next2 = entry2.Next
 					}
-
 				} else {
-					// 新队列没值
 					d.t2[entry.H&d.sizemask] = entry
 					d.t2[entry.H&d.sizemask].Next = nil
 				}
@@ -143,7 +85,7 @@ func (d *Dict) Set(key string, val string, t *Target) {
 	if entry := d.t1[subscript]; entry != nil {
 		for true {
 			if entry.Key == key {
-				fmt.Printf("数据已存在 key: %s \n", key)
+				fmt.Printf("Data already exists key: %s \n", key)
 				break
 			}
 			if entry.Next == nil {
