117f1f2cf489da2cc66abc1ee41d0593931803fb
Jeremy <acm_123@163.com>
2022-03-03 15:37:47 +0800

    fix

--- hotring/hotring.go
+++ hotring/hotring.go
@@ -1,6 +1,9 @@
 package hotring
 
-import "github.com/Jeremy-Run/zion/common"
+import (
+	"fmt"
+	"github.com/Jeremy-Run/zion/common"
+)
 
 type DictHR struct {
 	t0, t1   []*DictEntryHR
@@ -141,4 +144,37 @@ func (d *DictHR) Get(key string) string {
 		}
 	}
 	return ""
+}
+
+func ShowTable(table []*DictEntryHR) {
+	for slot, entry := range table {
+		if entry == nil {
+			fmt.Printf("slot number: %d, link: nil \n", slot)
+			continue
+		}
+		link := make([]*DictEntryHR, 0, 5)
+		t := 1
+		tag := entry.Tag
+		for true {
+			if entry.Tag == tag {
+				if t > 1 {
+					break
+				}
+				t <<= 1
+			}
+			link = append(link, entry)
+			entry = entry.Next
+		}
+
+		var linkStr string
+		lenLink := len(link)
+		for i, r := range link {
+			if i == lenLink-1 {
+				linkStr += fmt.Sprintf("{key:%s, value: %s, tag: %d} -> HEAD", r.Key, r.Val, r.Tag)
+			} else {
+				linkStr += fmt.Sprintf("{key:%s, value: %s, tag: %d} -> ", r.Key, r.Val, r.Tag)
+			}
+		}
+		fmt.Printf("slot number: %d, link: %v \n", slot, linkStr)
+	}
 }
--- hotring/hotring_test.go
+++ hotring/hotring_test.go
@@ -7,39 +7,6 @@ import (
 	"time"
 )
 
-func showAllDB(db *DictHR) {
-	for slot, entry := range db.t0 {
-		if entry == nil {
-			fmt.Printf("slot number: %d, link: nil \n", slot)
-			continue
-		}
-		link := make([]*DictEntryHR, 0, 5)
-		t := 1
-		tag := entry.Tag
-		for true {
-			if entry.Tag == tag {
-				if t > 1 {
-					break
-				}
-				t <<= 1
-			}
-			link = append(link, entry)
-			entry = entry.Next
-		}
-
-		var linkStr string
-		lenLink := len(link)
-		for i, r := range link {
-			if i == lenLink-1 {
-				linkStr += fmt.Sprintf("{key:%s, value: %s, tag: %d} -> HEAD", r.Key, r.Val, r.Tag)
-			} else {
-				linkStr += fmt.Sprintf("{key:%s, value: %s, tag: %d} -> ", r.Key, r.Val, r.Tag)
-			}
-		}
-		fmt.Printf("slot number: %d, link: %v \n", slot, linkStr)
-	}
-}
-
 const Key = "hello%d"
 const Value = "world%d"
 
@@ -48,7 +15,7 @@ func Test_Set(t *testing.T) {
 	for i := 0; i <= 10; i++ {
 		db.Set(fmt.Sprintf(Key, i), fmt.Sprintf(Value, i))
 	}
-	showAllDB(db)
+	ShowTable(db.t0)
 }
 
 func Test_Get(t *testing.T) {
@@ -60,12 +27,12 @@ func Test_Get(t *testing.T) {
 	}
 	rand.Seed(time.Now().UnixNano())
 
-	showAllDB(db)
+	ShowTable(db.t0)
 
 	k := fmt.Sprintf(Key, rand.Intn(max))
 	fmt.Printf("======================================\n")
 	fmt.Printf("key: %s, value: %s \n", k, db.Get(k))
 	fmt.Printf("======================================\n")
 
-	showAllDB(db)
+	ShowTable(db.t0)
 }
